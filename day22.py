d8a70df6f511f945275ba79bb9a945a95829ffdc
utkarsh vats <utkarshvats3434@gmail.com>
2026-05-20 11:11:40 +0530

    day22.py

--- day22.py
+++ day22.py
@@ -1,3 +1,26 @@
-l = [1, 2, 3, 4, 5]
-print(l)
-print(type(l))
+from tkinter.font import names
+
+
+marks= [1, 2, 3, 4, 5]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# print(marks[4])
+# print(marks[len(marks)-1]) 
+
+if 67 in marks:
+    print("67 is present in the list")
+else:
+    print("67 is not present in the list")
+# print(marks[0:3])
+# print(marks[1:4])
+# print(marks[0:3])
+print(marks[0:3:2]) #jump index 
+
+
+#list comprehension
+lst = [i for i in range(7)]
+print(lst)
---
+++ day23.py
@@ -0,0 +1,10 @@
+#list methods
+l = [1, 2, 83, 4, 5]
+print(l)
+# l.append(890)
+# l.sort()
+# l.sort(reverse=True)
+# l.reverse()
+print(l.index(83))
+print(l.count(83))
+print(l)
